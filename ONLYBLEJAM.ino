dd446c4e758b62e586094b651f2d18cd52bd51de
itslittleshaheer1 <z9z1z2z3z4z5z@gmail.com>
2024-10-06 23:52:05 +0500

    Update ONLYBLEJAM.ino

--- ONLYBLEJAM.ino
+++ ONLYBLEJAM.ino
@@ -8,7 +8,7 @@ RF24 radio(1, 2);
 #define CHANNELS  64
 int channel[CHANNELS];
 
-int  line;
+int line;
 
 const uint8_t num_channels = 64;
 int values[num_channels];
@@ -30,49 +30,51 @@ void setup()
   SPI.setClockDivider(SPI_CLOCK_DIV2);
   SPI.setBitOrder(MSBFIRST);
 
-  pinMode(CE,OUTPUT);
+  pinMode(CE, OUTPUT);
   disable();
 
   powerUp();
 
-  setRegister(_NRF24_EN_AA,0x0);
-  setRegister(_NRF24_RF_SETUP,0x0F);
+#define _NRF24_EN_AA 0x01
+#define _NRF24_CONFIG 0x00
+#define _NRF24_RF_SETUP 0x06
+#define _NRF24_RPD 0x09
 
   Serial.println("Starting Jamming...");
 }
 
 void setRegister(byte r, byte v)
 {
-  PORTB &=~_BV(2);
-  SPI.transfer((r&0x1F)|0x20);
+  digitalWrite(CE, LOW); // Use digitalWrite instead of manipulating PORTB
+  SPI.transfer((r & 0x1F) | 0x20);
   SPI.transfer(v);
-  PORTB |= _BV(2);
+  digitalWrite(CE, HIGH); // Use digitalWrite instead of manipulating PORTB
 }
 
 void powerUp(void)
 {
-  setRegister(_NRF24_CONFIG,getRegister(_NRF24_CONFIG)|0x02);
+  setRegister(_NRF24_CONFIG, getRegister(_NRF24_CONFIG) | 0x02);
   delayMicroseconds(130);
 }
 
 void powerDown(void)
 {
-  setRegister(_NRF24_CONFIG,getRegister(_NRF24_CONFIG)&~0x02);
+  setRegister(_NRF24_CONFIG, getRegister(_NRF24_CONFIG) & ~0x02);
 }
 
 void enable(void)
 {
-  PORTB |= _BV(1);
+  digitalWrite(CE, HIGH); // Use digitalWrite instead of manipulating PORTB
 }
 
 void disable(void)
 {
-  PORTB &=~_BV(1);
+  digitalWrite(CE, LOW); // Use digitalWrite instead of manipulating PORTB
 }
 
 void setRX(void)
 {
-  setRegister(_NRF24_CONFIG,getRegister(_NRF24_CONFIG)|0x01);
+  setRegister(_NRF24_CONFIG, getRegister(_NRF24_CONFIG) | 0x01);
   enable();
 
   delayMicroseconds(100);
@@ -82,7 +84,7 @@ void jammer() {
   const char text[] = "xxxxxxxxxxxxxxxx"; // send the noise
   for (int i = ((channels * 5) + 1); i < ((channels * 5) + 23); i++) {
     radio.setChannel(i);
-    radio.write( & text, sizeof(text));
+    radio.write(&text, sizeof(text));
   }
 }
 

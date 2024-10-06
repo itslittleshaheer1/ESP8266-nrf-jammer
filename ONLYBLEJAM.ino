41c819912bff0ca68c7827c3635b9be3d916a2fb
itslittleshaheer1 <z9z1z2z3z4z5z@gmail.com>
2024-10-06 23:43:25 +0500

    Update ONLYBLEJAM.ino

--- ONLYBLEJAM.ino
+++ ONLYBLEJAM.ino
@@ -5,61 +5,89 @@
 #define CE  1
 RF24 radio(1, 2);
 
-void setup() {
+#define CHANNELS  64
+int channel[CHANNELS];
+
+int  line;
+
+const uint8_t num_channels = 64;
+int values[num_channels];
+int channels = 0;
+const byte address[6] = "00001";
+const int num_reps = 50;
+bool jamming = true;
+
+void setup()
+{
   Serial.begin(57600);
-  
-  // NRF24L01 setup
+
   radio.begin();
+  radio.startListening();
   radio.stopListening();
-  
+
   SPI.begin();
   SPI.setDataMode(SPI_MODE0);
   SPI.setClockDivider(SPI_CLOCK_DIV2);
   SPI.setBitOrder(MSBFIRST);
-  
-  pinMode(CE, OUTPUT);
+
+  pinMode(CE,OUTPUT);
   disable();
+
   powerUp();
 
-  setRegister(_NRF24_EN_AA, 0x0);
-  setRegister(_NRF24_RF_SETUP, 0x0F);
-  
-  Serial.println("NRF24L01 Initialized");
+  setRegister(_NRF24_EN_AA,0x0);
+  setRegister(_NRF24_RF_SETUP,0x0F);
+
+  Serial.println("Starting Jamming...");
 }
 
-void loop() {
-  jammer();
-  delay(100); // Adjust delay as needed
-}
-
-void setRegister(byte r, byte v) {
-  digitalWrite(CE, LOW);
-  SPI.transfer((r & 0x1F) | 0x20);
+void setRegister(byte r, byte v)
+{
+  PORTB &=~_BV(2);
+  SPI.transfer((r&0x1F)|0x20);
   SPI.transfer(v);
-  digitalWrite(CE, HIGH);
+  PORTB |= _BV(2);
 }
 
-void powerUp() {
-  setRegister(_NRF24_CONFIG, getRegister(_NRF24_CONFIG) | 0x02);
+void powerUp(void)
+{
+  setRegister(_NRF24_CONFIG,getRegister(_NRF24_CONFIG)|0x02);
   delayMicroseconds(130);
 }
 
-void disable() {
-  digitalWrite(CE, LOW);
+void powerDown(void)
+{
+  setRegister(_NRF24_CONFIG,getRegister(_NRF24_CONFIG)&~0x02);
+}
+
+void enable(void)
+{
+  PORTB |= _BV(1);
+}
+
+void disable(void)
+{
+  PORTB &=~_BV(1);
+}
+
+void setRX(void)
+{
+  setRegister(_NRF24_CONFIG,getRegister(_NRF24_CONFIG)|0x01);
+  enable();
+
+  delayMicroseconds(100);
 }
 
 void jammer() {
-  const char text[] = "xxxxxxxxxxxxxxxx"; // Send the noise
-  for (int i = 0; i < 23; i++) { // Adjust based on your needs
+  const char text[] = "xxxxxxxxxxxxxxxx"; // send the noise
+  for (int i = ((channels * 5) + 1); i < ((channels * 5) + 23); i++) {
     radio.setChannel(i);
-    radio.write(&text, sizeof(text));
+    radio.write( & text, sizeof(text));
   }
 }
 
-byte getRegister(byte r) {
-  digitalWrite(CE, LOW);
-  SPI.transfer(r & 0x1F);
-  byte result = SPI.transfer(0);
-  digitalWrite(CE, HIGH);
-  return result;
+void loop()
+{
+  jammer();
+  delay(100);
 }

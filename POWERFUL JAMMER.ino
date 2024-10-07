6fe31e37095a9903e1d021649a4599a206c0ee9c
itslittleshaheer1 <z9z1z2z3z4z5z@gmail.com>
2024-10-07 23:10:09 +0500

    Update POWERFUL JAMMER.ino

--- POWERFUL JAMMER.ino
+++ POWERFUL JAMMER.ino
@@ -1,116 +1,183 @@
 #include <SPI.h>
-#include <nRF24L01.h>
 #include <RF24.h>
+#include <nRF24L01.h>
 
-#define CE  1
-RF24 radio(1, 2);
+#define CE_PIN D1 // GPIO 5
+#define CSN_PIN D2 // GPIO 4
 
-#define CHANNELS  80 // Cover the entire 2.4GHz band
+RF24 radio(CE_PIN, CSN_PIN);
+
+#define CHANNELS  64
 int channel[CHANNELS];
 
-int line;
+char grey[] = " .:-=+*aRW";
 
-const uint8_t num_channels = 80;
-int values[num_channels];
+// NRF24L01 Register Definitions
+#define _NRF24_CONFIG      0x00
+#define _NRF24_EN_AA       0x01
+#define _NRF24_EN_RXADDR   0x02  // Added for enabling RX addresses
+#define _NRF24_SETUP_AW    0x03  // Added for address width setup
+#define _NRF24_SETUP_RETR  0x04  // Added for retransmission settings
+#define _NRF24_RF_CH       0x05
+#define _NRF24_RF_SETUP    0x06
+#define _NRF24_STATUS      0x07
+#define _NRF24_OBSERVE_TX  0x08
+#define _NRF24_RPD         0x09  // Receive Power Detector
+#define _NRF24_RTX_ADDR    0x0A
+#define _NRF24_RX_ADDR_P0  0x0B
+#define _NRF24_RX_ADDR_P1  0x0C
+#define _NRF24_RX_ADDR_P2  0x0D
+#define _NRF24_RX_ADDR_P3  0x0E
+#define _NRF24_RX_ADDR_P4  0x0F
+#define _NRF24_RX_ADDR_P5  0x10
+#define _NRF24_TX_ADDR     0x10
+#define _NRF24_RX_PW_P0    0x11
+#define _NRF24_RX_PW_P1    0x12
+#define _NRF24_RX_PW_P2    0x13
+#define _NRF24_RX_PW_P3    0x14
+#define _NRF24_RX_PW_P4    0x15
+#define _NRF24_RX_PW_P5    0x16
+#define _NRF24_FIFO_STATUS  0x17
+#define _NRF24_DYNPD       0x1C
+#define _NRF24_FEATURE     0x1D
+
+#define BT1 D4 // GPIO 2
+#define BT2 D3 // GPIO 0
+
+byte count;
 int channels = 0;
-const byte address[6] = "00001";
-const int num_reps = 50;
-bool jamming = true;
-
-void setup()
-{
-  Serial.begin(57600);
-
-  radio.begin();
-  radio.startListening();
-  radio.stopListening();
-
-  SPI.begin();
-  SPI.setDataMode(SPI_MODE0);
-  SPI.setClockDivider(SPI_CLOCK_DIV2);
-  SPI.setBitOrder(MSBFIRST);
-
-  pinMode(CE, OUTPUT);
-  disable();
-
-  powerUp();
-
-#define _NRF24_EN_AA 0x01
-#define _NRF24_CONFIG 0x00
-#define _NRF24_RF_SETUP 0x06
-#define _NRF24_RPD 0x09
-
-  Serial.println("Starting Jamming...");
-}
-
-void setRegister(byte r, byte v)
-{
-  digitalWrite(CE, LOW); // Use digitalWrite instead of manipulating PORTB
-  SPI.transfer((r & 0x1F) | 0x20);
-  SPI.transfer(v);
-  digitalWrite(CE, HIGH); // Use digitalWrite instead of manipulating PORTB
-}
+bool jamming = false; // Initialize jamming to false
 
 byte getRegister(byte r) {
-  digitalWrite(CE, LOW); // Use digitalWrite instead of manipulating PORTB
-  SPI.transfer((r & 0x1F) | 0x00); // Note the 0x00 instead of 0x20
-  byte v = SPI.transfer(0x00); // Send a dummy byte to receive the value
-  digitalWrite(CE, HIGH); // Use digitalWrite instead of manipulating PORTB
-  return v;
+    byte c;
+    digitalWrite(CSN_PIN, LOW);
+    c = SPI.transfer(r & 0x1F);
+    c = SPI.transfer(0);
+    digitalWrite(CSN_PIN, HIGH);
+    return c;
 }
 
-void powerUp(void)
-{
-  setRegister(_NRF24_CONFIG, getRegister(_NRF24_CONFIG) | 0x02);
-  delayMicroseconds(130);
+void setRegister(byte r, byte v) {
+    digitalWrite(CSN_PIN, LOW);
+    SPI.transfer((r & 0x1F) | 0x20);
+    SPI.transfer(v);
+    digitalWrite(CSN_PIN, HIGH);
 }
 
-void powerDown(void)
-{
-  setRegister(_NRF24_CONFIG, getRegister(_NRF24_CONFIG) & ~0x02);
+void setup() {
+    Serial.begin(57600);
+    radio.begin();
+    radio.startListening();
+    radio.stopListening();
+
+    pinMode(BT1, INPUT_PULLUP);
+    pinMode(BT2, INPUT_PULLUP);
+
+    attachInterrupt(digitalPinToInterrupt(BT1), pressBt01, FALLING);
+    attachInterrupt(digitalPinToInterrupt(BT2), pressBt02, FALLING);
+
+    for (count = 0; count < CHANNELS; count++) {
+        channel[count] = 0;
+    }
+
+    Serial.println("Starting 2.4GHz Scanner ...");
+    Serial.println();
+    Serial.println("Channel Layout");
+
+    SPI.begin();
+    SPI.setDataMode(SPI_MODE0);
+    SPI.setClockDivider(SPI_CLOCK_DIV2);
+    SPI.setBitOrder(MSBFIRST);
+
+    pinMode(CE_PIN, OUTPUT);
+    disable();
+    powerUp();
+    setRegister(_NRF24_EN_AA, 0x0);
+    setRegister(_NRF24_RF_SETUP, 0x0F); // Set RF setup
 }
 
-void enable(void)
-{
-  digitalWrite(CE, HIGH); // Use digitalWrite instead of manipulating PORTB
+void powerUp(void) {
+    setRegister(_NRF24_CONFIG, getRegister(_NRF24_CONFIG) | 0x02);
+    delayMicroseconds(130);
 }
 
-void disable(void)
-{
-  digitalWrite(CE, LOW); // Use digitalWrite instead of manipulating PORTB
+void disable(void) {
+    digitalWrite(CE_PIN, LOW);
 }
 
-void setRX(void)
-{
-  setRegister(_NRF24_CONFIG, getRegister(_NRF24_CONFIG) | 0x01);
-  enable();
+void enable(void) {
+    digitalWrite(CE_PIN, HIGH);
+}
 
-  delayMicroseconds(100);
+void scanChannels(void) {
+    disable();
+    for (int j = 0; j < 255; j++) {
+        for (int i = 0; i < channels; i++) {
+            setRegister(_NRF24_RF_CH, (128 * i) / channels);
+            enable();
+            delayMicroseconds(40);
+            disable();
+            if (getRegister(_NRF24_RPD) > 0) channel[i]++;
+        }
+    }
+}
+
+void outputChannels(void) {
+    int norm = 0;
+    for (int i = 0; i < CHANNELS; i++)
+        if (channel[i] > norm) norm = channel[i];
+
+    Serial.print('|');
+    for (int i = 0; i < CHANNELS; i++) {
+        int pos;
+        if (norm != 0) pos = (channel[i] * 10) / norm;
+        else pos = 0;
+
+        if (pos == 0 && channel[i] > 0) pos++;
+        if (pos > 9) pos = 9;
+
+        Serial.print(grey[pos]);
+        channel[i] = 0;
+    }
+    Serial.print("| ");
+    Serial.println(norm);
 }
 
 void jammer() {
-  const int channels = 80; // Cover the entire 2.4GHz band
-  const int frequency_step = 5; // Use a 5MHz frequency step
-
-  for (int i = 0; i < channels; i++) {
-    int frequency = 2400 + (i * frequency_step); // Calculate the frequency
-    radio.setChannel(frequency);
-    radio.setPALevel(RF24_PA_HIGH); // Set the transmission power to high
-
-    // Generate a pseudorandom noise (PN) sequence
-    uint8_t pn_sequence[32];
-    for (int j = 0; j < 32; j++) {
-      pn_sequence[j] = (uint8_t)random(256);
+    const char text[] = "xxxxxxxxxxxxxxxx"; // Noise data to send
+    for (int i = 0; i < 128; i++) {
+        radio.setChannel(i); // Jam all channels from 0 to 127
+        radio.write(&text, sizeof(text));
     }
-
-    // Transmit the PN sequence
-    radio.write(pn_sequence, 32);
-    delayMicroseconds(10);
-  }
 }
 
-void loop()
-{
-  jammer();
-  delay(100);
+void pressBt01() {
+    static unsigned long last_interrupt_time = 0;
+    unsigned long interrupt_time = millis();
+    if (interrupt_time - last_interrupt_time > 200) {
+        if (channels < 13) {
+            channels++;
+        } else {
+            channels = 0;
+        }
+    }
+    last_interrupt_time = interrupt_time;
+}
+
+void pressBt02() {
+    jamming = !jamming; // Toggle jamming state
+    delay(200); // Debounce delay
+}
+
+void loop() {
+    Serial.print("Channel: ");
+    Serial.println(channels + 1);
+
+    if (jamming) {
+        Serial.print("JAMMING ALL CHANNELS ");
+        jammer(); // Call the jamming function
+    } else {
+        scanChannels();
+        outputChannels();
+    }
 }
